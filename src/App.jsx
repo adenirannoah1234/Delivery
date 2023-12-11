deb67e6863feae4c992a50939446dadf33106e1d
adenirannoah1234 <adenirannoah9@gmail.com>
2023-12-11 09:39:53 +0100

    initial changes

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Navbar from './components/navbar/Navbar';
+import Dashboard from './pages/dashboard/Dashboard';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Dashboard />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/bar/RightSideBar.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+const RightSideBar = () => {
+  return (
+    <section>
+      <div>
+        <div style={{ backgroundColor: 'white' }}>
+          <p>4,510</p>
+          <p>Total Items</p>
+        </div>
+        <div></div>
+      </div>
+      <div>
+        <div>
+          <p>45.234</p>
+          <p>Total Orders</p>
+        </div>
+        <div></div>
+      </div>
+      <div>
+        <div>
+          <p>1.325</p>
+          <p>Total Restaurant</p>
+        </div>
+        <div></div>
+      </div>
+      <div>
+        <div>
+          <p>105.434</p>
+          <p>Total Users</p>
+        </div>
+        <div></div>
+      </div>
+      <div>
+        <div>
+          <p>1.752</p>
+          <p>Total Drivers</p>
+        </div>
+        <div></div>
+      </div>
+    </section>
+  );
+};
+
+export default RightSideBar;
